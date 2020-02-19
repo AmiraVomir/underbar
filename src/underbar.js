b71d2d699f81c2de7a1d6eab40ce9ec6c141fa6a
Hieu Ho <hihho@ucdavis.edu>
2020-02-18 16:03:31 -0800

    Add map and pluck functions

--- src/underbar.js
+++ src/underbar.js
@@ -126,6 +126,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var arr = [];
+    _.each(collection, function (element) {
+        arr.push(iterator(element));
+    });
+    return arr;
   };
 
   /*
