60f999e3bc385f597aadd1e4c00861ef9e193e22
Hieu Ho <hihho@ucdavis.edu>
2020-02-20 14:11:03 -0800

    Add _flatten function

--- src/underbar.js
+++ src/underbar.js
@@ -394,6 +394,17 @@
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    var flatten = [];
+    (function isNested(nestedArray) {
+      for (var i = 0; i < nestedArray.length; i++) {
+        if (Array.isArray(nestedArray[i])) {
+          isNested(nestedArray[i]);
+        } else {
+          flatten.push(nestedArray[i]);
+        }
+      }
+    }) (nestedArray);
+    return flatten;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
